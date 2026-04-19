e0347632c071293dfe40c5bac2a5d1811e09989b
unknown <jaimebernardossantos@gmail.com>
2026-04-19 18:44:05 +0200

    Mi primer commit: versión inicial del proyecto

---
+++ app/src/main/java/com/oscarjaime/gymtraker/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.ivandepaz.boilerplate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Text
+import com.ivandepaz.boilerplate.ui.theme.BoilerplateTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BoilerplateTheme {
+                Text("BoilerPlate")
+            }
+        }
+    }
+}
